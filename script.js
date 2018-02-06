5136d2a24df6947f8cfdd10383e2d9c13040a6eb
akashdeep2001 <33459654+akashdeep2001@users.noreply.github.com>
2018-02-06 14:30:11 -0600

    update x+y variable

--- script.js
+++ script.js
@@ -3,17 +3,28 @@ function setup() {
 }
 
 function draw() {
+  background(255);
+
+  let x = 400;
+  let y = 400;
   //body
-  line(200,200, 200, 400)
-  
+
+  line(x,y, x, y+200);
+
   // head of the stickman with colour
-  fill(255,255,255)
-  ellipse(200, 200, 100, 100)
+  fill(255,255,255);
+  ellipse(x, y, 100, 100);
 
   // hat
-  fill(255, 0,0)
-  rect(150, 120, 100, 30)
-  rect(175,70,50,50)
+  fill(255, 0,0);
+  rect(x-50, y-80, 100, 30);
+  rect(x-25,y-130,50,50);
 
+  //arms
+  line(x-50,y+100,x+50,y+100);
+
+  //legs
+  line(x,y+200,x-50,y+250);
+  line(x,y+200,x+50,y+250);
 
 }
