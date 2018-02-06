7ec3d40800fcded72772701559572b2b9ca8c47f
akashdeep2001 <33459654+akashdeep2001@users.noreply.github.com>
2018-02-06 14:58:10 -0600

    test

--- script.js
+++ script.js
@@ -13,32 +13,34 @@ function draw() {
   drawStickman(mouseX, mouseY);
   drawStickman(x, y)
 }
-  function keyPressed() {
-    if (key === 'w' || key == 'W') {
-      y = y - 10;
-    }
 
+function keyPressed() {
+  if (key === 'w' || key == 'W') {
+    y = y - 10;
   }
 
-  function drawStickman(x, y) {
-    //body
 
-    line(x, y, x, y + 200);
+}
 
-    // head of the stickman with colour
-    fill(255, 255, 255);
-    ellipse(x, y, 100, 100);
+function drawStickman(x, y) {
+  //body
 
-    // hat
-    fill(255, 0, 0);
-    rect(x - 50, y - 80, 100, 30);
-    rect(x - 25, y - 130, 50, 50);
+  line(x, y, x, y + 200);
 
-    //arms
-    line(x - 50, y + 100, x + 50, y + 100);
+  // head of the stickman with colour
+  fill(255, 255, 255);
+  ellipse(x, y, 100, 100);
 
-    //legs
-    line(x, y + 200, x - 50, y + 250);
-    line(x, y + 200, x + 50, y + 250);
+  // hat
+  fill(255, 0, 0);
+  rect(x - 50, y - 80, 100, 30);
+  rect(x - 25, y - 130, 50, 50);
 
-  }
+  //arms
+  line(x - 50, y + 100, x + 50, y + 100);
+
+  //legs
+  line(x, y + 200, x - 50, y + 250);
+  line(x, y + 200, x + 50, y + 250);
+
+}
