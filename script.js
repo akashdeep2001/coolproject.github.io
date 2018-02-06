3672de2026c238988c3e46c3160663015bc8b8e5
akashdeep2001 <33459654+akashdeep2001@users.noreply.github.com>
2018-02-06 14:41:54 -0600

    draw stick

--- script.js
+++ script.js
@@ -5,26 +5,30 @@ function setup() {
 function draw() {
   background(255);
 
-  let x = mouseX;
-  let y = mouseY;
+  drawStickman(mouseX, mouseY)
+
+
+}
+
+function drawStickman(x, y) {
   //body
 
-  line(x,y, x, y+200);
+  line(x, y, x, y + 200);
 
   // head of the stickman with colour
-  fill(255,255,255);
+  fill(255, 255, 255);
   ellipse(x, y, 100, 100);
 
   // hat
-  fill(255, 0,0);
-  rect(x-50, y-80, 100, 30);
-  rect(x-25,y-130,50,50);
+  fill(255, 0, 0);
+  rect(x - 50, y - 80, 100, 30);
+  rect(x - 25, y - 130, 50, 50);
 
   //arms
-  line(x-50,y+100,x+50,y+100);
+  line(x - 50, y + 100, x + 50, y + 100);
 
   //legs
-  line(x,y+200,x-50,y+250);
-  line(x,y+200,x+50,y+250);
+  line(x, y + 200, x - 50, y + 250);
+  line(x, y + 200, x + 50, y + 250);
 
 }
