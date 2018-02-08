7b45b55c6e5537796d197d58ea59fcba7fd2e6fe
akashdeep2001 <33459654+akashdeep2001@users.noreply.github.com>
2018-02-08 15:19:28 -0600

    keybord and mouse finshed

--- keyboardandmouse/sketch.js
+++ keyboardandmouse/sketch.js
@@ -1,34 +1,34 @@
-// p5js template project - replace with project title
-// Dan Schellenberg - replace with your name
-// Feb 2, 2018 - replace with the date
+// mouse and keyboard interaction
+// Dan Schellenberg
+// Feb 8, 2018
 
-// global variables
-let gear;
 
-// the preload function guarentees that the code inside the function is
-// executed before the rest of the program runs -- helpful for things
-// like loading images (since JS is asynchronous)
-function preload() {
-  gear = loadImage("images/gear.png");
-}
-
-// the setup function will only run once (before the draw loop begins)
-// this is where you want to set up the environment (size of canvas, etc)
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  document.addEventListener("contextmenu", event => event.preventDefault())
 }
 
 function draw() {
-  background(255);
 
-  image(gear, 0, 0);
+}
 
-  stroke(0);
-  line(0, 0, 200, 200);
-
-  fill(0, 255, 0, 100);
+function mousePressed() {
   noStroke();
+  fill(random(255), random(255), random(255), random(255));
 
-  rect(mouseX, mouseY, 100, 300);
-  ellipse(400, 150, 300, 200);
+  if (mouseButton === LEFT) {
+    rect(mouseX, mouseY, random(50, 150), random(50, 150));
+  }
+  else if (mouseButton === RIGHT) {
+    ellipse(mouseX, mouseY, random(50, 150), random(50, 150));
+  }
+}
+
+function keyPressed() {
+  if (key === 'w' || key === 'W') {
+    background(255);
+  }
+  else if (key === 'b' || key === 'B') {
+    background(0);
+  }
 }
