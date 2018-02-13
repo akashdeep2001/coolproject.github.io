58b1f12394772298c2e23bc1b75d582641cdcd4c
Singh3802389 <3802389@sps-student.sk.ca>
2018-02-13 15:29:59 -0600

    2/13/2018

--- interactivescene/sketch.js
+++ interactivescene/sketch.js
@@ -1,13 +1,37 @@
 // p5js template project - replace with project title
 // Dan Schellenberg - replace with your name
 // Feb 2, 2018 - replace with the date
+let fillShape = "white";
 
-
-var img;
-function preload() {
-  img = loadImage('images/gear.png');
-}
 function setup() {
-  imageMode(CENTER);
-  image(img,100  ,100,[windowWidth],[windowHeight])
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  drawSquare();
+}
+
+
+function keyTyped() {
+  if (key === "w") {
+
+    fillShape = "white";
+  }
+  else if (key === "g") {
+    fillShape = "black";
+  }
+  else if (key === "b") {
+    fillShape = "blue";
+  }
+  else if (key === "g") {
+    fillShape = "green";
+  }
+}
+
+function drawSquare() {
+  if (key === "d") {
+    fill(fillShape);
+    rectMode(CENTER);
+    rect(width / 2, height / 2, 200, 200);
+  }
 }
