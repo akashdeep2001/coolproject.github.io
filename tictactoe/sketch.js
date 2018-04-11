6393c3a4d869ceede11e336a516912d6860f81a9
Singh3802389 <3802389@sps-student.sk.ca>
2018-04-11 14:37:39 -0600

    tic tae toe

---
+++ tictactoe/sketch.js
@@ -0,0 +1,46 @@
+let rows = 3;
+let cols = 3;
+let grid;
+let cellSize;
+
+
+function setup() {
+  createCanvas(600, 600);
+  cellSize = width / cols;
+}
+
+function draw() {
+  background(255);
+  grid = createRandom2dArray(cols, rows);
+  displayGrid();
+}
+
+function displayGrid() {
+  for (let x=0; x<cols; x++) {
+    for (let y=0; y<rows; y++) {
+      if (grid[x][y] === 0) {
+        fill(0);
+      }
+      else {
+        fill(255);
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function createRandom2dArray(cols, rows) {
+  let randomGrid = [];
+  for (let x=0; x<cols; x++) {
+    randomGrid.push([]);
+    for (let y=0; y<rows; y++) {
+      if (random(100) < 50) {
+        randomGrid[x].push(0);
+      }
+      else {
+        randomGrid[x].push(1);
+      }
+    }
+  }
+  return randomGrid;
+}
